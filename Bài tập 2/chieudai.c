49f9c10b37694ae04ac94cb218e0e7da85723a36
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2020-12-03 16:36:24 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int met,cm,mm;
+    printf("met(m) , xentimet(cm), milimet(mm)\n");
+    scanf("%d",&met);
+    printf("%d %d %d ",met,met * 100,met * 1000);
+    return 0;
+}
+    
+    1
