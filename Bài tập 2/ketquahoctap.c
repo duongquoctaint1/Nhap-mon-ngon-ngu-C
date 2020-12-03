bf602c7f2a0720a413b900b40a597bdd963a8d3e
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2020-12-03 16:24:19 +0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float Van,Toan,Diem Trung Binh;
+    printf("điểm văn, điểm toán, điểm trung bình\n");
+    scanf("%f%f",&V,&T);
+    printf("%f %f %f ",Van,Toan,Diem Trung Binh = (V + T)/2);
+    return 0;
+}
+    
