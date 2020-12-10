2e4aef40ffed7a9031b27c24b1c9169d7a0ccccc
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2020-12-10 15:45:08 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int a,b;
+	printf("nhap a ");
+	scanf ("%d" , &a);
+	printf("nhap b ");
+	scanf ("%d" , &b);
+	if(a > b) { 
+	     printf("a>b");
+    }else if (a == b){
+         printf("a = b");
+    }else {     
+         printf("a<b");
+    }  
+	getchar();
+	return 0;
+}
