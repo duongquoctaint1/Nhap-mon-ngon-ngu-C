dd33b661df6d1c9a669b7a5302df3ff58e4bc001
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2020-12-17 16:52:47 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -7,29 +7,20 @@ int main(void) {
   scanf("%d", &thang);
   switch(thang){
     case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
     printf("31 ngay");break;
     case 2:
     printf("28 ngay");break;
-    case 3:
-    printf("31 ngay");break;
     case 4:
-    printf("30 ngay");break;
-    case 5:
-    printf("31 ngay");break;
     case 6:
-    printf("30 ngay");break;
-    case 7:
-    printf("31 ngay");break;
-    case 8:
-    printf("31 ngay");break;
     case 9:
-    printf("30 ngay");break;
-    case 10:
-    printf("31 ngay");break;
     case 11:
     printf("30 ngay");break;
-    case 12:
-    printf("31 ngay");break;
       default: 
   printf("nhap 12 thang trong nam");
   break;
