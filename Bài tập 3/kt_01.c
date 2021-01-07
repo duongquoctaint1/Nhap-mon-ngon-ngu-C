dceaceb79bf0d01c5f2b8900b3ffbdb5b999e675
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2021-01-07 16:13:29 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main (){
   int a,b;
-  for(a=1;a<=10;a++){
-    printf ("bang cuu chuong %d\n",a);
-    for(b=1;b<=10;b++)
-      printf("%d*%d=%d\n",a,b,a*b);
-
- }
+  printf ("bang cuu chuong \n",a);
+    printf("bang 2\n");
+      printf("2 * 1= 2\n");
+      printf ("2 * 2= 4\n");
+      printf ("2 * 3= 6\n");
+    printf("bang 3\n");
+      printf ("3 * 1= 3\n");
+      printf ("3 * 2= 6\n");
+      printf ("3 * 3= 9\n");
+    printf("bang 4\n");  
+      printf ("4 * 1= 4\n");
+      printf ("4 * 2= 8\n");
+      printf ("4 * 3= 12\n");
 return 0;
   }
