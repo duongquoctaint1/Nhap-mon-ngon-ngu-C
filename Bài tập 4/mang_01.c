dcb1c23e8a6048a8c9bc0ca3c31ad24fa0807c6e
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2021-01-14 14:52:26 +0700

    Create mang_01.c

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){    
+    int a[4]={3,5,7,2};
+    int i,sum=0;
+    for (i=0;i<4;i++){
+    printf("Phan tu thu %d: %d\n",i+1,a[i]);
+sum+=a[i];}
+printf("Tong cac phan tu trong mang: %d",sum);
+  return 0;
+}
