869166eb675cc645540c8d1db80091d2e78eaf51
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2021-01-22 09:32:16 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -8,3 +8,16 @@ return 0;
 }
 int sum(int a, int b, int c){
 return a+b+c;}
+
+
+
+#include <stdio.h>
+
+void sum(int a, int b, int c);// khai bao ham sum 
+int main(){
+int x= 1, y = 2, z = 3;
+int sum = x + y + z;
+printf ("Tong la %d", sum);
+}
+void sum(int a, int b, int c){
+}
