1ba6d4ddb42a5c074ede76c155fa275bf6eaece9
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2021-01-22 10:23:32 +0700

    Create ham_02.c

---
+++ Bài tập 5/ham_02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int kiem_tra_am_duong(int n);
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  if(kiem_tra_am_duong(n) > 0)
+      printf("n la so nguyen duong ");
+  else if (kiem_tra_am_duong(n) < 0)
+    printf("n la so nguyen am");
+  else 
+    printf("n = 0");
+  return 0;
+}
+int kiem_tra_am_duong(int n){
+int kiem_tra_am_duong;
+return n;
+}
