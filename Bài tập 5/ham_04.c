9ff9052273cc59885c0b5cd6285485d2fdc38f52
Đường Quốc Tài <72543349+duongquoctaint1@users.noreply.github.com>
2021-01-28 10:14:51 +0700

    Update ham_04.c

--- Bài tập 5/ham_04.c
+++ Bài tập 5/ham_04.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-int LN_n(int n, int arr[]){
-int lonnhat = 0;
+int LN_n(int n, int a[]){
+int LN_n = 0;
   for (int i = 0; i < n; i++){
-    if (arr[i] > lonnhat){
-      lonnhat = arr[i];}
+    if (a[i] > LN_n){
+      LN_n = a[i];}
 	  }
-  return lonnhat;}
-int giaithua (int n, int arr[]){
+  return LN_n;}
+int giaithua (int n, int a[]){
 int giaithua = 1;
   for (int i=0; i<n; i++){
-    giaithua *=  arr[i]; 
+    giaithua *=  a[i]; 
   }
   return giaithua;}
 int main (){
   printf ("nhap n = ");
   int n;
   scanf ("%d", &n);
-  int arr[n];
+  int a[n];
   for (int i=0; i<n; i++){
     printf ("gia tri cua mang %d = ", i);
-    scanf ("%d", &arr[i]);
+    scanf ("%d", &a[i]);
   }
-  printf ("gia tri cua phan tu lon nhat = %d\n", LN_n(n, arr));
-  printf ("giai thua cua n = %d", giaithua(n, arr));
+  printf ("gia tri cua phan tu lon nhat = %d\n", LN_n(n, a));
+  printf ("giai thua cua n = %d", giaithua(n, a));
 }
